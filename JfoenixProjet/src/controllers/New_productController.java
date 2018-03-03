8f6a89420d7be45291ccc7a23f5ec7a56d63d731
yveronne <veronneyepmo@gmail.com>
2018-03-03 17:57:46 +0100

    Initialisation

---
+++ JfoenixProjet/src/controllers/New_productController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Alain
+ */
+public class New_productController extends Controller implements Initializable {
+    
+    private Stage currentStage;
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    public Stage getCurrentStage() {
+        return currentStage;
+    }
+
+    public void setCurrentStage(Stage currentStage) {
+        this.currentStage = currentStage;
+    }
+    
+}
---
+++ JfoenixProjet/src/controllers/RegisterController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import java.awt.Button;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Alain
+ */
+public class RegisterController extends Controller implements Initializable {
+    
+    @FXML
+    private Button addbtn;
+    
+    private Stage currentStage;
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    public Stage getCurrentStage() {
+        return currentStage;
+    }
+
+    public void setCurrentStage(Stage currentStage) {
+        this.currentStage = currentStage;
+    }
+    
+}
